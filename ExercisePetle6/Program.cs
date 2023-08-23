823ee358a807588b623c45740b9b71292631586c
ACTFYZZ <ACTFYZZ@PL10006677>
2023-08-23 10:28:17 +0200

    Add project files.

---
+++ ExercisePetle6/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExercisePetle6
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            double a = 0;
+
+            //Calculation         
+            for (double i = 1; i <= 20; i++)
+            {
+                a = a + 1/i;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
